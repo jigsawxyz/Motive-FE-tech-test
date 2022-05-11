29919d82698907469ea813215fd5193b2df5fc93
Can Yilmaz <spawnedc@gmail.com>
2022-05-11 09:48:39 +0100

    Update group-transactions.js

--- group-transactions/group-transactions.js
+++ group-transactions/group-transactions.js
@@ -1,9 +1,12 @@
+// To be fetched from https://gist.githubusercontent.com/simeor/5dd52dccdf5c4b4183ab2f2e80728bec/raw/5d042617ce514687fedffba35ec04539cb173481/data.json
+const data = [];
+
 const groupTransactionsByMerchant = () => {
-  /// CODE GOES HERE ///
+  // CODE GOES HERE
 };
 
 const groupTransactionsByDate = () => {
-  /// CODE GOES HERE ///
+  //
 };
 
 module.exports = {
